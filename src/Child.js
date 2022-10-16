276a4aee0855af7a54f62378337ea41093690ea0
Brook Nega <brooknega21@gmail.com>
2022-10-16 14:47:43 -0400

    commit tree e022c1769b55c8056d99962e88c59edb06057886

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({ onChangeColor, color}) {
+  // console.log(onChangeColor)
+  return <div 
+  className="child" 
+  onClick={onChangeColor} 
+  style={{ backgroundColor: color}}/>;
 }
 
 export default Child;
